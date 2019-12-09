2946a0e0c30b596d346231df1b093278f9e1ed89
saam71 <2130725@my.ipleiria.pt>
2019-12-09 18:32:39 +0000

    Error Correction
    Proper singleton and import removal

--- src/android/TouchDrawActivity.java
+++ src/android/TouchDrawActivity.java
@@ -38,7 +38,6 @@ import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.UUID;
-import Manager;
 
 public class TouchDrawActivity extends Activity {
     public static final String DRAWING_RESULT_PARCELABLE = "drawing_result";
@@ -83,7 +82,7 @@ public class TouchDrawActivity extends Activity {
         if (intentExtras != null) {
             mBackgroundImageType = BackgroundImageType.values()[
                     intentExtras.getInt(BACKGROUND_IMAGE_TYPE, BackgroundImageType.COLOUR.ordinal())];
-                    
+
             //mBackgroundImageUrl = intentExtras.getString(BACKGROUND_IMAGE_URL, mBackgroundImageUrl);
             mBackgroundImageUrl = Manager.getInstance().inputData;
             
